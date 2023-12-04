b2d792790ba47260dbcf92228b09e2f07466e25e
jmatthews64 <jon.matthews64@gmail.com>
2023-12-04 15:48:21 -0500

    Updated page to display categories pulled from API

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
+import React, { useEffect, useState} from 'react';
+import axios from 'axios'
 import './App.css';
-import React, { useState} from 'react';
 
 function App() {
-  const [cats, setCats] = useState('')
+  const [cats, setCats] = useState([])
+
+  useEffect(() => {
+    console.log("useFX is running")
+    axios
+      .get('https://opentdb.com/api_category.php')
+      .then((categories) => setCats(categories.data.trivia_categories))
+  }, [])
+
   return (
     <div className="App">
       <h1 className="title">Welcome to Trivia!</h1>
       <h3 className="title">To begin, select a category and press start.</h3>
+      <div className='categoryList'>
+      {cats.map((cat) => (
+        <Category 
+          key={cat.id}
+          name={cat.name}
+        />
+      ))}
+      </div>
       <div className='selectorFrame'>
         <label for="category-selector" className='selectorLabel'>Please choose your Trivia Category:<br></br></label>
         <select className='category-selector'>
@@ -18,6 +34,14 @@ function App() {
       </div>
     </div>
   );
+}
+
+function Category(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+    </div>
+  )
 }
 
 export default App;
