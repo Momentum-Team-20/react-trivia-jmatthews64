df97d61dc2e3c9f77fbd0d437b8e0f92414d08fd
jmatthews64 <jon.matthews64@gmail.com>
2023-12-04 15:50:50 -0500

    Updated page to display categories in dropdown list

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -17,17 +17,18 @@ function App() {
       <h1 className="title">Welcome to Trivia!</h1>
       <h3 className="title">To begin, select a category and press start.</h3>
       <div className='categoryList'>
-      {cats.map((cat) => (
-        <Category 
-          key={cat.id}
-          name={cat.name}
-        />
-      ))}
+      
       </div>
       <div className='selectorFrame'>
         <label for="category-selector" className='selectorLabel'>Please choose your Trivia Category:<br></br></label>
         <select className='category-selector'>
           <option value="">---Please Select a Category---</option>
+          {cats.map((cat) => (
+        <Category 
+          key={cat.id}
+          name={cat.name}
+        />
+      ))}
         </select>
         <br></br>
         <button for='category-selector' className="categoryButton">Submit</button>
@@ -38,9 +39,7 @@ function App() {
 
 function Category(props) {
   return (
-    <div>
-      <h2>{props.name}</h2>
-    </div>
+    <option value={props.key}>{props.name}</option>
   )
 }
 
